f577d9faab6364d4a3d0b624d0315cde6ae74ffb
C Crognier <christophe.crognier@gmail.com>
2023-04-21 14:27:38 +0200

    New version with header and hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import Home from './pages/Home'
-import Portfolio from './pages/Portfolio'
-import Contact from './pages/Contact'
+import { BrowserRouter } from 'react-router-dom'
+import { Header, Hero, Contact, Who, Works, Experience, StarCanvas } from './components'
 
 function App() {
   return (
-    <main className="app transition-all ease-in">
-      <Home />
-      <Portfolio />
-      {/* <Contact /> */}
-    </main>
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Header />
+            <Hero />
+          </div>
+          <Who />
+          <Experience />
+          <Works />
+        </div>
+        <div className="relative z-0">
+          <Contact />
+          <StarCanvas />
+        </div>
+      </BrowserRouter>
   )
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,18 @@ import CustomButton from "./CustomButton"
 import Hero from "./Hero"
 import Who from "./Who"
 import Works from "./Works"
+import Contact from "./Contact"
+import Header from "./Header"
+import Experience from "./Experience"
+import StarCanvas from "./StarCanvas"
 
 export {
     CustomButton,
     Hero,
     Who,
     Works,
+    Contact,
+    Header,
+    Experience,
+    StarCanvas
 };
